b076f8e07e5a2cd2f9abe57c9e9027a2196fa189
PouPiXiaoBaoZi <2681971282@qq.com>
2026-07-24 14:46:00 +0800

    完成练习07

--- exercises/07_loop/main.cpp
+++ exercises/07_loop/main.cpp
@@ -5,11 +5,22 @@
 // READ: 纯函数 <https://zh.wikipedia.org/wiki/%E7%BA%AF%E5%87%BD%E6%95%B0>
 static unsigned long long fibonacci(int i) {
     // TODO: 为缓存设置正确的初始值
-    static unsigned long long cache[96], cached;
-    // TODO: 设置正确的循环条件
-    for (; false; ++cached) {
-        cache[cached] = cache[cached - 1] + cache[cached - 2];
+    // cache[0] = 0, cache[1] = 1。cached 记录当前已经算到了哪一个下标
+    static unsigned long long cache[96] = {0, 1};
+    static int cached = 1; 
+
+    // 安全检查：如果请求的下标 i 已经计算过了，直接返回缓存结果
+    if (i <= cached) {
+        return cache[i];
     }
+
+    // TODO: 设置正确的循环条件
+    // 从下一个未计算的位置 (cached + 1) 开始，一直计算到需要的 i
+    for (int j = cached + 1; j <= i; ++j) {
+        cache[j] = cache[j - 1] + cache[j - 2];
+    }
+    
+    cached = i; // 更新已缓存的最大索引
     return cache[i];
 }
 
