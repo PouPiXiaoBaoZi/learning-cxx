297cd3d69166e02883a520e33879145398369b6b
PouPiXiaoBaoZi <2681971282@qq.com>
2026-07-24 14:19:42 +0800

    完成练习06 (按要求补全)

--- exercises/06_array/main.cpp
+++ exercises/06_array/main.cpp
@@ -2,7 +2,7 @@
 
 // READ: 数组 <https://zh.cppreference.com/w/cpp/language/array>
 
-unsigned long long arr[90] = {0, 1};
+unsigned long long arr[90]{0, 1};
 unsigned long long fibonacci(int i) {
     switch (i) {
         case 0:
@@ -11,7 +11,7 @@ unsigned long long fibonacci(int i) {
             return 1;
         default:
             // TODO: 补全三目表达式缺失的部分
-            return arr[i] ? arr[i] : (arr[i] = fibonacci(i-1) + fibonacci(i-2));
+            return arr[i] ? arr[i] : (arr[i] = fibonacci(i - 1) + fibonacci(i - 2));
     }
 }
 
@@ -21,6 +21,6 @@ int main(int argc, char **argv) {
     // ---- 不要修改以下代码 ----
     ASSERT(fibonacci(2) == 1, "fibonacci(2) should be 1");
     ASSERT(fibonacci(20) == 6765, "fibonacci(20) should be 6765");
-    ASSERT(fibonacci(80) == 23416728348467685ULL, "fibonacci(80) should be 23416728348467685");
+    ASSERT(fibonacci(80) == 23416728348467685, "fibonacci(80) should be 23416728348467685");
     return 0;
 }
